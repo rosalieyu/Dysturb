3390e9ce4ef758957a44e2e12b80f3ec41e37bc6
therewasaguy <jason.sigal@gmail.com>
2015-12-09 01:58:54 -0500

    temporary fix for tying audio to meshes

--- js/audioModule.js
+++ js/audioModule.js
@@ -2,10 +2,9 @@
  *  create audio elements
  */
 
-// return an array of audio elements
-var makeAudio = function(pathArray, cam, movieScreenArrayMeshes) {
+// initialize audio listener and return an array of audio elements
+var makeAudio = function(pathArray, cam, meshes) {
 	var arr = [];
-
 	var webAudioEnabled = typeof( (window.AudioContext || window.webkitAudioContext) ) != 'undefined';
 
 	// if there is no web audio, use html5 audio elements
@@ -20,13 +19,38 @@ var makeAudio = function(pathArray, cam, movieScreenArrayMeshes) {
 		var listener = new THREE.AudioListener();
 		cam.add( listener );
 
-		for (var i = 0; i < pathArray.length; i++) {
+		for (var i = 0; i < meshes.length; i++) {
 			var snd = new THREE.Audio(listener);
 			snd.load(pathArray[i]);
-			movieScreenArrayMeshes[i].add(snd)
+			meshes[i].add(snd);
 			arr.push(snd);
 		}
 	}
 
 	return arr;
 };
+
+// return an audio element
+function addAudioToMesh(pathToAudio, mesh, cam) {
+	var webAudioEnabled = typeof( (window.AudioContext || window.webkitAudioContext) ) != 'undefined';
+
+	// if there is no web audio, use html5 audio elements
+	if (!webAudioEnabled) {
+		return new Audio(pathArray[i]);
+	}
+
+	else {
+		var listener = cam.children.filter(
+			function(obj) {
+				return obj instanceof THREE.AudioListener;
+			}
+		)[0];
+
+		var snd = new THREE.Audio(listener);
+		snd.load(pathToAudio);
+
+		mesh.add(snd);
+
+		return snd;
+	}
+}
