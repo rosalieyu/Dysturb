e01e40ef42fa415951b2d0e8296fbbe263d9e659
therewasaguy <jason.sigal@gmail.com>
2015-12-09 02:16:43 -0500

    Audio3d object handles all audio functions

--- js/audioModule.js
+++ js/audioModule.js
@@ -2,55 +2,55 @@
  *  create audio elements
  */
 
-// initialize audio listener and return an array of audio elements
-var makeAudio = function(pathArray, cam, meshes) {
-	var arr = [];
+
+var Audio3d = function() {
+
 	var webAudioEnabled = typeof( (window.AudioContext || window.webkitAudioContext) ) != 'undefined';
 
-	// if there is no web audio, use html5 audio elements
-	if (!webAudioEnabled) {
-		for (var i = 0; i < pathArray.length; i++) {
-			arr.push(new Audio(pathArray[i]));
-		}
-	}
+	// initialize audio listener and return an array of audio elements
+	Audio3d.initAudio = function(pathArray, cam, meshes) {
+		var arr = [];
 
-	// if there is web audio, use 3d panner
-	else {
-		var listener = new THREE.AudioListener();
-		cam.add( listener );
-
-		for (var i = 0; i < meshes.length; i++) {
-			var snd = new THREE.Audio(listener);
-			snd.load(pathArray[i]);
-			meshes[i].add(snd);
-			arr.push(snd);
-		}
-	}
-
-	return arr;
-};
-
-// return an audio element
-function addAudioToMesh(pathToAudio, mesh, cam) {
-	var webAudioEnabled = typeof( (window.AudioContext || window.webkitAudioContext) ) != 'undefined';
-
-	// if there is no web audio, use html5 audio elements
-	if (!webAudioEnabled) {
-		return new Audio(pathArray[i]);
-	}
-
-	else {
-		var listener = cam.children.filter(
-			function(obj) {
-				return obj instanceof THREE.AudioListener;
+		// if there is no web audio, use html5 audio elements
+		if (!webAudioEnabled) {
+			for (var i = 0; i < pathArray.length; i++) {
+				arr.push( new Audio(pathArray[i]) );
 			}
-		)[0];
+		}
 
-		var snd = new THREE.Audio(listener);
-		snd.load(pathToAudio);
+		// if there is web audio, use 3d panner
+		else {
+			Audio3d.listener = new THREE.AudioListener();
+			cam.add( Audio3d.listener );
 
-		mesh.add(snd);
+			for (var i = 0; i < meshes.length; i++) {
+				var snd = new THREE.Audio( Audio3d.listener );
+				snd.load(pathArray[i]);
+				meshes[i].add(snd);
+				arr.push(snd);
+			}
+		}
 
-		return snd;
+		return arr;
+	};
+
+	// return an audio element
+	Audio3d.addAudioToMesh = function(pathToAudio, mesh) {
+
+		if (!webAudioEnabled) {
+			return new Audio(pathToAudio);
+		}
+
+		else {
+			var snd = new THREE.Audio(Audio3d.listener);
+			snd.load(pathToAudio);
+
+			mesh.add(snd);
+
+			return snd;
+		}
 	}
+
 }
+
+Audio3d();
